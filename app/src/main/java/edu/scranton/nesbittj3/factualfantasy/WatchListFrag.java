33e0d1028a220560bca1ccf0d0f8b0b5efcd17f3
John Nesbitt <john.nesbitt@scranton.edu>
2021-03-15 16:03:39 -0400

    3/15 Commit - Basic webscraping working - app is able to populate the template "Player Watchlist" with QB names from ESPN.com

--- app/src/main/java/edu/scranton/nesbittj3/factualfantasy/WatchListFrag.java
+++ app/src/main/java/edu/scranton/nesbittj3/factualfantasy/WatchListFrag.java
@@ -1,11 +1,15 @@
 package edu.scranton.nesbittj3.factualfantasy;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -17,7 +21,8 @@ import com.android.volley.toolbox.Volley;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
-import org.w3c.dom.Document;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import edu.scranton.nesbittj3.factualfantasy.model.ExamplePlayer;
 
@@ -52,7 +57,7 @@ public class WatchListFrag extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager((context)));
         recyclerView.addItemDecoration(new DividerItemDecoration(context, LinearLayoutManager.VERTICAL));
         //temporary just to test
-        playersList = new ArrayList<>();
+        //playersList = new ArrayList<>();
 
         //pRequestQueue = Volley.newRequestQueue(context);
         //parseJSON();
@@ -63,8 +68,10 @@ public class WatchListFrag extends Fragment {
         //playersList.add(examplePlayer);
         adapter = new WatchListAdapter(context, playersList);
         recyclerView.setAdapter(adapter);
+        Content content = new Content();
+        content.execute();
 
-        getActivity().runOnUiThread(new Runnable()
+        /*getActivity().runOnUiThread(new Runnable()
         {
             ExamplePlayer examplePlayer = new ExamplePlayer("Deshaun Watson", null, false);
 
@@ -90,10 +97,10 @@ public class WatchListFrag extends Fragment {
                 }
             }
 
-        });
+        });*/
         //adapter = new WatchListAdapter(context, playersList);
         //recyclerView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+        //adapter.notifyDataSetChanged();
         //recyclerView.setAdapter(adapter);
 
         return view;
@@ -118,6 +125,53 @@ public class WatchListFrag extends Fragment {
         //adapter.resetSelected();
     }
 
+    private class Content extends AsyncTask<Void, Void, Void>{
 
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                String url = "https://www.espn.com/nfl/stats/player/_/table/passing/sort/passingYards/dir/desc";
+
+                Document doc = Jsoup.connect(url).get();
+
+
+                Elements data = doc.select("td.Table__TD");
+                //Elements data2 = doc.select("h1.Image__Wrapper"); //Image__Wrapper--relative
+                int size = data.size();
+
+                for(int i = 0; i< size; i ++){
+                    /*String imgUrl = data.select("h1.Image__Wrapper") // Image__Wrapper--relative
+                            .select("img")
+                            .eq(i)
+                            .attr("alt");*/
+                    String athleteName = data.select("a.AnchorLink")
+                            .select("a")
+                            .eq(i)
+                            .text();
+                    playersList.add(new ExamplePlayer(null, athleteName, false));
+                    //Log.d("items", "name: " + athleteName );
+                }
+            }catch(IOException e){
+                e.printStackTrace();;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
 
 }
